8607713c54b702a08e86b3bac8c09d05ab686237
Soma Holiday <soma@somaholiday.com>
2020-05-01 15:14:48 -0700

    Add plugin-node-resolve and plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
 export default {
@@ -14,7 +16,8 @@ export default {
     {
       file: pkg.browser,
       format: 'iife', // immediately invoked function expression (browser style)
-      name: 'PhaseTwo_JS', // the global which can be used in a browser
+      name: 'Phasetwo', // the global which can be used in a browser
     },
   ],
+  plugins: [resolve(), commonjs()],
 };
