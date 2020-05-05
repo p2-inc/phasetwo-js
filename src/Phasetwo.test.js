9b3bcee20de55f3296dc985d62e55d81a34e1505
Soma Holiday <soma@somaholiday.com>
2020-05-04 17:56:00 -0700

    Test mocking of Keycloak

--- src/Phasetwo.test.js
+++ src/Phasetwo.test.js
@@ -1,4 +1,11 @@
+import Keycloak from 'keycloak-js';
 import Phasetwo from './Phasetwo';
+
+jest.mock('keycloak-js');
+
+beforeEach(() => {
+  Keycloak.mockClear();
+});
 
 describe('Phasetwo', () => {
   it('getAugment returns supplied augment value', () => {
@@ -9,5 +16,12 @@ describe('Phasetwo', () => {
     });
 
     expect(p2.getAugment()).toEqual(testValue);
+  });
+
+  describe('Keycloak interactions', () => {
+    it('Keycloak gets created', () => {
+      const p2 = new Phasetwo();
+      expect(Keycloak).toHaveBeenCalled();
+    });
   });
 });
